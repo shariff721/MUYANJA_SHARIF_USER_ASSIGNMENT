6386290c593a879b52d2b8ad0111dc30525033de
sharif721 <smuyanja11@gmail.com>
2023-04-11 14:43:05 -0700

    new changes

--- muyanja_sharif_user.py
+++ muyanja_sharif_user.py
@@ -1,6 +1,6 @@
 
 class User:
-    def __init__(self, first_name, last_name, email, age, is_reward_member = False, gold_card_points = 200):
+    def __init__(self, first_name, last_name, email, age, is_reward_member = False, gold_card_points = 0):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
